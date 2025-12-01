647d36f6579db8b397d1cff875b6c579dcaff688
Paul Caporn <paulcaporn@pauls-mbp.home>
2025-12-01 09:14:31 +0000

    Tidy up

--- day1/day1.cpp
+++ day1/day1.cpp
@@ -17,25 +17,17 @@ int main() {
         int value = std::stoi(line.substr(1));
         int rots = value / 100;
         int rem = value % 100;
-        if (line[0] == 'L'){            
-            int newpos = (pos + 100 - rem) % 100;
-            if (newpos == 0){
-                part1Count++;
-                part2Count++;
-            } else if (pos != 0 && newpos > pos){
-                part2Count++;
-            }
-            pos = newpos;
-        } else {
-            int newpos = (pos + 100 + rem) % 100;
-            if (newpos == 0){
-                part1Count++;
-                part2Count++;
-            } else if (pos != 0 && newpos < pos){
+        int sign = (line[0] == 'L') ? -1 : 1;
+        int newpos = (pos + 100 + (sign * rem)) % 100;
+        if (newpos == 0){
+            part1Count++;
+            part2Count++;
+        } else if (pos != 0){
+            if (sign < 0 && newpos > pos || sign > 0 && newpos < pos){
                 part2Count++;
             }
-            pos = newpos;
         }
+        pos = newpos;        
         part2Count += rots;
     }
     std::cout << "Part1: " << part1Count << std::endl;
